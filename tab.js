606e26f5a52338588308e5d4c1eb119a9e88d564
youssef-nachar <156086960+youssef-nachar@users.noreply.github.com>
2026-05-22 08:21:17 +0300

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,12 +1,48 @@
 function showDashboardHome() {
+
     document.getElementById("dashboardHeader").style.display = "flex";
-    document.getElementById("newOrderTab").classList.add("hidden");
-    document.getElementById("teamNotesTab").classList.add("hidden");
-        document.getElementById("dsh").style.display="block";
-    document.getElementById("readyTab").classList.add("hidden");
-    document.querySelector(".kpis").classList.remove("hidden");
-    document.querySelector(".warehouse-container").classList.remove("hidden");
-    document.querySelector(".sales-order").classList.remove("hidden");
+
+    document.getElementById("newOrderTab")
+        .classList.add("hidden");
+
+    document.getElementById("teamNotesTab")
+        .classList.add("hidden");
+
+    document.getElementById("readyTab")
+        .classList.add("hidden");
+
+    // 🔥 أضف هذا
+    document.getElementById("returnTab")
+        .classList.add("hidden");
+
+    document.getElementById("dsh").style.display = "block";
+
+    document.querySelector(".kpis")
+        .classList.remove("hidden");
+
+    document.querySelector(".warehouse-container")
+        .classList.remove("hidden");
+
+    document.querySelector(".sales-order")
+        .classList.remove("hidden");
 }
+function loginAsWarehouse(warehouseName) {
 
+    const user = users.find(
+        u => u.warehouse === warehouseName
+    );
 
+    if (!user) {
+        alert("Warehouse user not found");
+        return;
+    }
+
+    // حفظ بيانات الدخول
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("currentWarehouse", user.warehouse);
+    localStorage.setItem("userWarehouse", user.warehouse);
+    localStorage.setItem("userRole", user.role);
+
+    // إعادة تحميل الصفحة
+    location.reload();
+}
